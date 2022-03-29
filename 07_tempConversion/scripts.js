6529083591c6aad72e6d86d481138f1b726d49a2
fritzjackhunt <essiet.aniekan@yahoo.com>
2022-03-30 01:34:07 +0300

    done exercise 7

---
+++ 07_tempConversion/scripts.js
@@ -0,0 +1,15 @@
+const ctof = function(value) {
+    y = 9 / 5;
+    console.log(y)
+
+    x = value * y;
+    console.log(x)
+
+    b = x + 32;
+    console.log(b)
+
+    g = Math.round(b * 10 ) / 10;
+    console.log(g)
+};
+
+ctof(-10)
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,27 @@
-const ftoc = function() {
+const ftoc = function(value) {
+  // Formula = (value - 32) * 5/9
+  subValue = value - 32;
 
+  divFormula = 5 / 9;
+
+  finalValue = subValue * divFormula;
+
+  roundValue = Math.round(finalValue * 10 ) / 10;
+  
+  return roundValue;
 };
 
-const ctof = function() {
+const ctof = function(value) {
+  // Formula (value * 9/5) + 32
+  divFormula = 9 / 5;
 
+  subValue = value * divFormula;
+
+  finalValue = subValue + 32;
+
+  roundValue = Math.round(finalValue * 10 ) / 10;
+  
+  return roundValue;
 };
 
 // Do not edit below this line
