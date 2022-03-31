5d10164201385699dbcc8569d99928020e88a1d0
fritzjackhunt <essiet.aniekan@yahoo.com>
2022-03-31 19:43:41 +0300

    done exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,5 +1,9 @@
-const getTheTitles = function() {
+const getTheTitles = function(arr) {
+  var val1 = arr[0].title
+  var val2 = arr[1].title
 
+  var finalArr = [val1, val2]
+  return finalArr
 };
 
 // Do not edit below this line
