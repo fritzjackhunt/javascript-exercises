842ed613300adea647ab188192409be813ea297f
fritzjackhunt <essiet.aniekan@yahoo.com>
2022-03-31 23:52:37 +0300

    done exercise 08

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,62 @@
-const add = function() {
-	
+const add = function(x, y) {
+	return x + y;
 };
 
-const subtract = function() {
-	
+const subtract = function(x, y) {
+	return x - y;
 };
 
-const sum = function() {
-	
+const sum = function([x, y, ...z]) {
+	const numbers = [x, y, ...z]
+
+  function myFunc(total, num) {
+      return total + num;
+  }
+
+  if (x === undefined) {
+    return 0;
+
+  } else if (y === undefined) {
+    return x;
+
+  } else {
+    var red = numbers.reduce(myFunc);
+    return red;
+  }
 };
 
-const multiply = function() {
+const multiply = function([x, y, ...z]) {
+	const numbers = [x, y, ...z];
 
+  function myFunc(total, num) {
+      return total * num;
+  }
+
+  if (x === undefined) {
+    return 0;
+
+  } else if (y === undefined) {
+    return x;
+
+  } else {
+    var red = numbers.reduce(myFunc);
+    return red;
+  }
 };
 
-const power = function() {
-	
+const power = function(x, y) {
+	return x ** y;
 };
 
-const factorial = function() {
-	
+const factorial = function(num) {
+	var result = num;
+  if (num === 0 || num === 1) 
+    return 1; 
+  while (num > 1) { 
+    num--;
+    result *= num;
+  }
+  return result;
 };
 
 // Do not edit below this line
