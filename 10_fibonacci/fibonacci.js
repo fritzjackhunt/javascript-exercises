3c364346d73f19245cc65250c9030502794de46a
fritzjackhunt <essiet.aniekan@yahoo.com>
2022-04-01 05:09:12 +0300

    done exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
+const fibonacci = function(num) {
 
+    if (num === "1") {
+        return 1;
+    }
+
+    if ( num < 0 ) {
+        return "OOPS";
+    }
+
+    if(num < 2) {
+        return num;
+    } else {
+        return fibonacci(num - 1) + fibonacci(num - 2);
+    } 
 };
 
 // Do not edit below this line
