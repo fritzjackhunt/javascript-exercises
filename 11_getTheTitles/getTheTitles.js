79f9ca19c0b987297d32b3a9617def199ccc4910
fritzjackhunt <essiet.aniekan@yahoo.com>
2022-04-02 14:59:03 +0300

    updated exercise 11

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,9 +1,6 @@
 const getTheTitles = function(arr) {
-  var val1 = arr[0].title
-  var val2 = arr[1].title
-
-  var finalArr = [val1, val2]
-  return finalArr
+  const titles = arr.map(arr => arr.title);
+  return titles
 };
 
 // Do not edit below this line
